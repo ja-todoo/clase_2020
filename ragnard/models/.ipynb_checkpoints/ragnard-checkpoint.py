7b465c4fb3ec2568628d806fb7885ba208755605
julian <ja@todoo.co>
2020-02-21 23:27:27 +0000

    julian

--- ragnard/models/.ipynb_checkpoints/ragnard-checkpoint.py
+++ ragnard/models/.ipynb_checkpoints/ragnard-checkpoint.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class ragnard(models.Model):
-#     _name = 'ragnard.ragnard'
-#     _description = 'ragnard.ragnard'
+ class ragnard(models.Model):
+     _name = 'ragnard.ragnard'
+     _description = ´'Esta clase almacena los usuarios ragnard.ragnard'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+     Nombre = fields.Char()
+     Cedula= fields.Integer()
+     Años = fields.Float(compute="_value_pc", store=True)
+     memo = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
